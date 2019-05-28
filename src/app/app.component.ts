0a6c321965563729517e5a309f00ac1ca4bce622
deanapeterson <dean.peterson.iconweb@gmail.com>
2019-05-28 11:18:22 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { interval, timer} from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  lenInMin = 1;
+  lenInSeconds = 0;
+  countdown = 0;
+  width = '0%';
+  timer$;
+  start () {
+    this.lenInSeconds = this.lenInMin * 60;
+    this.timer$ = timer(0, 1000)
+      .subscribe((count)=>{
+        this.countdown = count;
+        this.updateWidth();
+      })
+  }
+  updateWidth() {
+    const percentInt = Math.round((this.countdown / this.lenInSeconds) * 100);
+    this.width = percentInt.toString() + '%';
+    // this.width = Math.round( * 100 )  * .1;
+  }
 }
