67e4d0e807ac8ab82d831d3d50ff71b0f0034173
deanapeterson <dean.peterson.iconweb@gmail.com>
2019-05-28 12:25:35 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { interval, timer } from 'rxjs';
-import { takeWhile } from 'rxjs/operators/';
+import { Subject, interval, timer } from 'rxjs';
+import { takeWhile, takeUntil } from 'rxjs/operators/';
 
 @Component({
   selector: 'my-app',
@@ -12,17 +12,18 @@ export class AppComponent {
   lenInSeconds = 0;
   countdown = 0; // seconds
   percentComplete = 0;
-  remaining = 0;
+  remaining = '0:00';
   width = '0%';
   completed = false;
   timer$;
-  setLength(lenInMinutes){
+  reset$ = new Subject();
 
-  }
   start() {
     this.lenInSeconds = this.lenInMin * 60;
+    this.completed = false;
     this.timer$ = timer(0, 1000)
       .pipe(
+        takeUntil(this.reset$),
         takeWhile(() => {
           return this.percentComplete <= 100;
         })
@@ -31,27 +32,41 @@ export class AppComponent {
         this.countdown = count;
         this.updateWidth();
         this.updateRemaining();
-      }, () => this.onError(), () => this.onComplete());
+      }, () => {}, () => this.onComplete());
   }
   reset() {
     this.lenInSeconds = 0;
     this.countdown = 0;
     this.percentComplete = 0;
+    this.remaining = '0:00';
     this.width = '0%';
-    this.completed = false;
+    this.reset$.next();
   }
-  onError() { }
+
   onComplete() {
     this.completed = true;
+    this.reset();
   }
   updateWidth() {
     this.percentComplete = Math.round((this.countdown / this.lenInSeconds) * 100);
     this.width = this.percentComplete.toString() + '%';
-
-    // this.width = Math.round( * 100 )  * .1;
   }
-  updateRemaining() {
-    const result = (this.lenInSeconds - this.countdown);
-    this.remaining = result >= 0 ? result : 0;
+
+  private updateRemaining() {
+    const totalSecRemaining = (this.lenInSeconds - this.countdown);
+
+    if( totalSecRemaining < 0 ){
+      return '0:00';
+    }
+
+    const minRemaining = (Math.floor(totalSecRemaining / 60)).toString(); 
+    let secRemaining = (totalSecRemaining % 60).toString();
+
+    if(secRemaining.length === 1) {
+      secRemaining = '0' + secRemaining;
+    }
+
+
+    this.remaining = `${minRemaining}:${secRemaining}`;
   }
 }
