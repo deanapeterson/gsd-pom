eaf0a5ac4f6441c2b830366911c08b935637fbde
deanapeterson <deanapeterson@users.noreply.github.com>
2023-08-16 10:23:48 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,15 @@ import { Title } from '@angular/platform-browser';
 import { Observable, Subject, interval, timer } from 'rxjs';
 import { takeWhile, takeUntil, filter } from 'rxjs/operators/';
 
-
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   title = 'GTD-POM-ADD-Timer';
   selectedPreset: [] | null = null;
+  width = 300;
   constructor(private titleService: Title) {
     this.setTitle();
   }
@@ -31,59 +29,61 @@ export class AppComponent {
           return this.percentComplete <= 100;
         })
       )
-      .subscribe((count) => {
-        this.secElapsed = this.secElapsed + 1;
-        this.updateWidth();
-        this.updateRemaining();
-      }, () => { }, () => this.onComplete());
+      .subscribe(
+        (count) => {
+          this.secElapsed = this.secElapsed + 1;
+          this.updateWidth();
+          this.updateRemaining();
+        },
+        () => {},
+        () => this.onComplete()
+      );
   }
   setTitle(content = '') {
-    if( content ){
+    if (content) {
       content = content + ' | ';
     }
     this.titleService.setTitle(`${content}${this.title}`);
   }
 
-
-
   // https://devdocs.io/dom/notification
   notify() {
-
     // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
+    if (!('Notification' in window)) {
+      alert('This browser does not support desktop notification');
     }
 
     // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
+    else if (Notification.permission === 'granted') {
       // If it's okay let's create a notification
-      var notification = new Notification("Hi there!");
+      var notification = new Notification('Hi there!');
     }
 
     // Otherwise, we need to ask the user for permission
-    else if (Notification.permission !== "denied") {
+    else if (Notification.permission !== 'denied') {
       Notification.requestPermission().then(function (permission) {
         // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          var notification = new Notification("Times UP!!!!");
+        if (permission === 'granted') {
+          var notification = new Notification('Times UP!!!!');
         }
       });
     }
-
   }
   updateWidth() {
-    this.percentComplete = Math.round((this.secElapsed / this.lenInSeconds) * 100);
+    this.percentComplete = Math.round(
+      (this.secElapsed / this.lenInSeconds) * 100
+    );
     this.width = this.percentComplete.toString() + '%';
   }
 
   private updateRemaining() {
-    const totalSecRemaining = (this.lenInSeconds - this.secElapsed);
+    const totalSecRemaining = this.lenInSeconds - this.secElapsed;
 
     if (totalSecRemaining < 0) {
       return '0:00';
     }
 
-    const minRemaining = (Math.floor(totalSecRemaining / 60)).toString();
+    const minRemaining = Math.floor(totalSecRemaining / 60).toString();
     let secRemaining = (totalSecRemaining % 60).toString();
 
     this.setTitle(`${minRemaining}:${secRemaining}`);
